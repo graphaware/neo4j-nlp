4c7e36f563bb5c3b2275fcbe5326879e415d9139
alenegro81 <alenegro81@yahoo.it>
2016-09-19 14:01:32 +0200

    fix on NerDemo

--- src/main/java/com/graphaware/nlp/processor/stanford/NERDemo.java
+++ src/main/java/com/graphaware/nlp/processor/stanford/NERDemo.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package com.graphaware.nlp.processor;
+package com.graphaware.nlp.processor.stanford;
 
 /**
  *
