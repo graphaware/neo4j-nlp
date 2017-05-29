da0f9a87e69faef9a249b66e928c15f1cd42c0a5
Vlasta Kus <vlastimil.kus@gmail.com>
2017-05-29 15:18:34 +0200

    Adding GeneralModelParameters.

---
+++ src/main/java/com/graphaware/nlp/util/GenericModelParameters.java
@@ -0,0 +1,26 @@
+/*
+ * Copyright (c) 2013-2016 GraphAware
+ *
+ * This file is part of the GraphAware Framework.
+ *
+ * GraphAware Framework is free software: you can redistribute it and/or modify it under the terms of
+ * the GNU General Public License as published by the Free Software Foundation, either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details. You should have received a copy of
+ * the GNU General Public License along with this program.  If not, see
+ * <http://www.gnu.org/licenses/>.
+ */
+package com.graphaware.nlp.util;
+
+public class GenericModelParameters {
+  public static final String TRAIN_ALG  = "trainerAlg";
+  public static final String TRAIN_TYPE = "trainerType";
+  public static final String TRAIN_ITER = "iter";
+  public static final String TRAIN_CUTOFF = "cutoff";
+  public static final String TRAIN_THREADS = "threads";
+  public static final String TRAIN_ENTITYTYPE = "entityType";
+  public static final String VALIDATE_FOLDS = "nFolds";
+}
