ccfa80714730acd277b82f5cee8a0f9434508132
Alessandro Negro <alenegro81@yahoo.it>
2017-08-04 10:12:56 +0200

    fixing minor issues in the TextRank algo

--- src/main/java/com/graphaware/nlp/ml/textrank/PageRank.java
+++ src/main/java/com/graphaware/nlp/ml/textrank/PageRank.java
@@ -17,6 +17,7 @@ package com.graphaware.nlp.ml.textrank;
 
 import com.google.common.util.concurrent.AtomicDouble;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.Map;
 import org.neo4j.graphdb.GraphDatabaseService;
 import org.slf4j.Logger;
@@ -32,26 +33,31 @@ public class PageRank {
         this.database = database;
     }
 
-    public Map<Long, Double> run(Map<Long, Map<Long, CoOccurrenceItem>> coOccurrences, int iter, double dampFactor) {
+    public Map<Long, Double> run(Map<Long, Map<Long, CoOccurrenceItem>> coOccurrences, int iter, double dampFactor, double threshold) {
         Map<Long, Double> nodeWeights = initializeNodeWeights(coOccurrences);
         Map<Long, Double> pagerank = getInitializedPageRank(nodeWeights, dampFactor);
-        for (int iteration = 0; iteration < iter; iteration++) {
+        boolean thresholdHit = false;
+        for (int iteration = 0; iteration < iter && !thresholdHit; iteration++) {
             Map<Long, Double> prTemp = new HashMap<>();
             // calculate main part of the PR calculation, include weights of nodes and relationships
-            nodeWeights.keySet().stream().forEach((nodeIdExt) -> {                
+            nodeWeights.keySet().stream().forEach((nodeIdExt) -> {
                 AtomicDouble internalSum = new AtomicDouble(0.0);
                 nodeWeights.keySet().stream()
                         .filter((nodeIdInt) -> coOccurrences.containsKey(nodeIdInt)
                         && coOccurrences.get(nodeIdInt).containsKey(nodeIdExt))
                         .forEach((nodeIdInt) -> {
-                            Map<Long, CoOccurrenceItem> coOccurrentTags = coOccurrences.get(nodeIdInt); 
+                            Map<Long, CoOccurrenceItem> coOccurrentTags = coOccurrences.get(nodeIdInt);
                             //Can be optimized
                             double totalWeightSum = coOccurrentTags.values().stream().map(item -> item.getCount()).mapToDouble(Integer::intValue).sum();
-                            internalSum.addAndGet((nodeWeights.get(nodeIdInt)/totalWeightSum) * pagerank.get(nodeIdInt));
+                            internalSum.addAndGet(((1.0d * coOccurrentTags.get(nodeIdExt).getCount()) / totalWeightSum) * pagerank.get(nodeIdInt));
                         });
-                double newPrValue = (1 - dampFactor) + dampFactor*internalSum.get();
+                double newPrValue = (1 - dampFactor) + dampFactor * internalSum.get();
                 prTemp.put(nodeIdExt, newPrValue);
             });
+            thresholdHit = checkThreshold(pagerank, prTemp, threshold);
+            if (thresholdHit) {
+                LOG.warn("Threshold hit after " + iter + " iterations");
+            }
             // finish page rank computation and store it to the final list
             nodeWeights.keySet().stream().forEach((nodeIdExt) -> {
                 pagerank.put(nodeIdExt, prTemp.get(nodeIdExt));
@@ -60,76 +66,8 @@ public class PageRank {
         return pagerank;
     }
 
-//    public Set<Object[]> runOld(Long id, /*String nodeType,*/ String relType, String relWeight, int iter, double damp) {
-//        Map<String, Object> params = new HashMap<>();
-//        params.put("id", id);
-//        //params.put("nodeType", nodeType);
-//        params.put("relType", relType);
-//        params.put("relWeight", relWeight);
-//
-//        // get node weights (tf-idf)
-//        initializeNodeWeightsOld(params);
-//
-//        // get adjacancy matrix
-//        initializeAdjacencyMatrix(params);
-//
-//        // run it
-//        final int nNodes = nodeWeights.size();
-//        getInitializedPageRank(nNodes, damp);
-//        double max_node_w = nodeWeights.entrySet().stream().max(Map.Entry.comparingByValue(Double::compareTo)).get().getValue();
-//        double sum_node_w = nodeWeights.entrySet().stream().mapToDouble(e -> e.getValue()).sum();
-//        double[] prTemp = new double[nNodes];
-//        for (int it = 0; it < iter; it++) {
-//            for (int ii = 0; ii < nNodes; ii++) {
-//                prTemp[ii] = 0.;
-//            }
-//
-//            // calculate main part of the PR calculation, include weights of nodes and relationships
-//            for (int i = 0; i < nNodes; i++) {
-//                double sum_locnodes_w = 0.;
-//                double max_locnodes_w = 0.;
-//                int n_locNodes = 0;
-//                for (int j = 0; j < nNodes; j++) {
-//                    if (nodes_rels_sumW.get(j) != 0) {
-//                        //prTemp[i] += pagerank.get(j) * adjMatrix.get(i).get(j) / nodes_rels_sumW.get(j);
-//                        prTemp[i] += nodeWeights.get(nodes_idxToNode.get(j)) * pagerank.get(j) * adjMatrix.get(i).get(j) / nodes_rels_sumW.get(j);
-//                        if (adjMatrix.get(i).get(j) != 0) {
-//                            sum_locnodes_w += nodeWeights.get(nodes_idxToNode.get(j));
-//                            n_locNodes += 1;
-//                        }
-//                        if (adjMatrix.get(i).get(j) != 0 && max_locnodes_w < nodeWeights.get(nodes_idxToNode.get(j))) {
-//                            max_locnodes_w = nodeWeights.get(nodes_idxToNode.get(j));
-//                        }
-//                    }
-//                }
-//                prTemp[i] /= sum_locnodes_w != 0 ? sum_locnodes_w / n_locNodes : 1;
-//                //prTemp[i] /= max_locnodes_w!=0 ? max_locnodes_w : 1;
-//                //prTemp[i] /= sum_node_w/nNodes;
-//                ////prTemp[i] /= max_node_w;
-//            }
-//
-//            // finish page rank computation and store it to the final list
-//            for (int i = 0; i < nNodes; i++) {
-//                prTemp[i] = (1 - damp) / nNodes + damp * prTemp[i];
-//                //prTemp[i] = (1-damp) + damp * prTemp[i];
-//                //prTemp[i] = (1-damp) * nodeWeights.get(nodes_idxToNode.get(i))/sum_node_w + damp * prTemp[i];
-//                pagerank.set(i, prTemp[i]);
-//            }
-//
-//        } // iterations
-//
-//        LOG.debug("PageRank results:");
-//        Set<Object[]> result = new HashSet<>();
-//        for (int i = 0; i < nNodes; i++) {
-//            LOG.debug(nodes_idxToNode.get(i).getProperty("id") + ": " + pagerank.get(i));
-//            result.add(new Object[]{nodes_idxToNode.get(i), pagerank.get(i), nodeWeights.get(nodes_idxToNode.get(i))});
-//        }
-//        LOG.debug("Max node-weight (tf-idf) value: " + max_node_w);
-//        return result;
-//    }
-
-    //Currently only with one as for the article
     private Map<Long, Double> initializeNodeWeights(Map<Long, Map<Long, CoOccurrenceItem>> coOccurrences) {
+        
         Map<Long, Double> nodeInitialWeights = new HashMap<>();
         coOccurrences.entrySet().stream().forEach((coOccurrence) -> {
             coOccurrence.getValue().entrySet().stream().forEach((entry) -> {
@@ -140,170 +78,26 @@ public class PageRank {
         return nodeInitialWeights;
     }
 
-//    private void initializeNodeWeightsOld(Map<String, Object> params) {
-//        String query = "MATCH (doc:AnnotatedText)\n"
-//                + "WITH count(doc) as documentsCount\n"
-//                + "MATCH (a:AnnotatedText)-[:CONTAINS_SENTENCE]->(:Sentence)-[ht:HAS_TAG]->(t:Tag)\n"
-//                + "WHERE a.id = {id} and EXISTS((t)-[:" + params.get("relType") + "]-(:Tag))\n" // we're interested in this subset of tags only
-//                + "WITH t, sum(ht.tf) as tf, documentsCount\n"
-//                + "MATCH (a:AnnotatedText)-[:CONTAINS_SENTENCE]->(:Sentence)-[:HAS_TAG]->(t)\n"
-//                + "RETURN t as tag, tf, count(distinct a) as docCountForTag, documentsCount\n";
-//
-//        nodeWeights = new HashMap<>();
-//        Result res;
-//        try (Transaction tx = database.beginTx();) {
-//            res = database.execute(query, params);
-//            tx.success();
-//        } catch (Exception e) {
-//            LOG.error("Error while initializing node weights: ", e);
-//            return;
-//        }
-//        while (res != null && res.hasNext()) {
-//            Map<String, Object> next = res.next();
-//            Node tag = (Node) next.get("tag");
-//            long tf = (Long) next.get("tf"); //getDoubleValue(next.get("tf"));
-//
-//            long docCount = (long) next.get("documentsCount");
-//            long docCountTag = (long) next.get("docCountForTag");
-//            double idf = Double.valueOf(Math.log10(Double.valueOf(1.0f * docCount / docCountTag).doubleValue())).floatValue();
-//
-//            nodeWeights.put(tag, tf * idf);
-//            LOG.debug("Tag = " + tag.getProperty("value") + ": tf = " + tf + ", idf = " + idf + " (docCountTag = " + docCountTag + "), tf*idf = " + tf * idf);
-//        }
-//    }
-
-//    private void initializeAdjacencyMatrix() {
-//        String query = "MATCH (a:AnnotatedText)-[:CONTAINS_SENTENCE]->(s:Sentence)-[:HAS_TAG]->(t:Tag)-[c:" + params.get("relType") + "]->(t2:Tag)\n"
-//                + "WHERE a.id = {id} and EXISTS((t2)<-[:HAS_TAG]-(:Sentence)<-[:CONTAINS_SENTENCE]-(a))\n"
-//                + "WITH distinct c as c\n"
-//                + "MATCH (t:Tag)-[c]->(t2:Tag)\n"
-//                + "RETURN t as Tag1, t2 as Tag2, (case c." + params.get("relWeight") + " when null then 1.0f else c." + params.get("relWeight") + " end) as wr\n";
-//        //+ "ORDER BY Tag1\n";
-//
-//        final int nNodes = nodeWeights.size();
-//        indexNodes(nodeWeights);
-//
-//        // initialize adjacancy matrix
-//        adjMatrix = new HashMap<Integer, List<Double>>();
-//        for (int i = 0; i < nNodes; i++) {
-//            List pom = new LinkedList<Double>();
-//            for (int j = 0; j < nNodes; j++) {
-//                pom.add(j, 0.);
-//            }
-//            adjMatrix.put(i, pom);
-//        }
-//
-//        // run query and process results
-//        Result res;
-//        try (Transaction tx = database.beginTx();) {
-//            res = database.execute(query, params);
-//            tx.success();
-//        } catch (Exception e) {
-//            LOG.error("Error while initializing adjacency matrix: ", e);
-//            return;
-//        }
-//        while (res != null && res.hasNext()) {
-//            Map<String, Object> next = res.next();
-//            Node tag1 = (Node) next.get("Tag1");
-//            Node tag2 = (Node) next.get("Tag2");
-//            double wr = Double.valueOf((Long) next.get("wr")); //getDoubleValue(next.get("wr"));
-//
-//            // when relationship direction is not important:
-//            if (!nodes_nodeToIdx.containsKey(tag1)) {
-//                LOG.error("Tag node " + tag1.getProperty("id") + " was not indexed (nNodes_indexed = " + nNodes + "). Check node-weights query.");
-//                continue;
-//            }
-//            if (!nodes_nodeToIdx.containsKey(tag2)) {
-//                LOG.error("Tag node " + tag2.getProperty("id") + " was not indexed (nNodes_indexed = " + nNodes + "). Check node-weights query.");
-//                continue;
-//            }
-//            adjMatrix.get(nodes_nodeToIdx.get(tag1)).set(nodes_nodeToIdx.get(tag2), wr);
-//            adjMatrix.get(nodes_nodeToIdx.get(tag2)).set(nodes_nodeToIdx.get(tag1), wr);
-//        }
-//
-//        // calculate sum of relationship weights for each node
-//        nodes_rels_sumW = new LinkedList<Double>();
-//        for (int i = 0; i < nNodes; i++) {
-//            nodes_rels_sumW.add(i, adjMatrix.get(i).stream().mapToDouble(Double::doubleValue).sum());
-//        }
-//    }
-//
-//    private void initializeAdjacencyMatrix(Map params) {
-//        String query = "MATCH (a:AnnotatedText)-[:CONTAINS_SENTENCE]->(s:Sentence)-[:HAS_TAG]->(t:Tag)-[c:" + params.get("relType") + "]->(t2:Tag)\n"
-//                + "WHERE a.id = {id} and EXISTS((t2)<-[:HAS_TAG]-(:Sentence)<-[:CONTAINS_SENTENCE]-(a))\n"
-//                + "WITH distinct c as c\n"
-//                + "MATCH (t:Tag)-[c]->(t2:Tag)\n"
-//                + "RETURN t as Tag1, t2 as Tag2, (case c." + params.get("relWeight") + " when null then 1.0f else c." + params.get("relWeight") + " end) as wr\n";
-//        //+ "ORDER BY Tag1\n";
-//
-//        final int nNodes = nodeWeights.size();
-//        indexNodes(nodeWeights);
-//
-//        // initialize adjacancy matrix
-//        adjMatrix = new HashMap<Integer, List<Double>>();
-//        for (int i = 0; i < nNodes; i++) {
-//            List pom = new LinkedList<Double>();
-//            for (int j = 0; j < nNodes; j++) {
-//                pom.add(j, 0.);
-//            }
-//            adjMatrix.put(i, pom);
-//        }
-//
-//        // run query and process results
-//        Result res;
-//        try (Transaction tx = database.beginTx();) {
-//            res = database.execute(query, params);
-//            tx.success();
-//        } catch (Exception e) {
-//            LOG.error("Error while initializing adjacency matrix: ", e);
-//            return;
-//        }
-//        while (res != null && res.hasNext()) {
-//            Map<String, Object> next = res.next();
-//            Node tag1 = (Node) next.get("Tag1");
-//            Node tag2 = (Node) next.get("Tag2");
-//            double wr = Double.valueOf((Long) next.get("wr")); //getDoubleValue(next.get("wr"));
-//
-//            // when relationship direction is not important:
-//            if (!nodes_nodeToIdx.containsKey(tag1)) {
-//                LOG.error("Tag node " + tag1.getProperty("id") + " was not indexed (nNodes_indexed = " + nNodes + "). Check node-weights query.");
-//                continue;
-//            }
-//            if (!nodes_nodeToIdx.containsKey(tag2)) {
-//                LOG.error("Tag node " + tag2.getProperty("id") + " was not indexed (nNodes_indexed = " + nNodes + "). Check node-weights query.");
-//                continue;
-//            }
-//            adjMatrix.get(nodes_nodeToIdx.get(tag1)).set(nodes_nodeToIdx.get(tag2), wr);
-//            adjMatrix.get(nodes_nodeToIdx.get(tag2)).set(nodes_nodeToIdx.get(tag1), wr);
-//        }
-//
-//        // calculate sum of relationship weights for each node
-//        nodes_rels_sumW = new LinkedList<Double>();
-//        for (int i = 0; i < nNodes; i++) {
-//            nodes_rels_sumW.add(i, adjMatrix.get(i).stream().mapToDouble(Double::doubleValue).sum());
-//        }
-//    }
-//
-//    private void indexNodes(Map<Node, Double> map) {
-//        nodes_nodeToIdx = new HashMap<Node, Integer>();
-//        nodes_idxToNode = new HashMap<Integer, Node>();
-//        int i = 0;
-//        for (Node key : map.keySet()) {
-//            nodes_nodeToIdx.put(key, i);
-//            nodes_idxToNode.put(i, key);
-//            i++;
-//        }
-//    }
-
     private Map<Long, Double> getInitializedPageRank(Map<Long, Double> nodeWeights, double damp) {
         Map<Long, Double> pageRank = new HashMap<>();
         int n = nodeWeights.size();
         nodeWeights.entrySet().stream().forEach((item) -> {
-                //pageRank.put(item.getKey(), (1. - damp) / n);
-                pageRank.put(item.getKey(), 1.);
+            pageRank.put(item.getKey(), (1. - damp) / n);
         });
-        
+
         return pageRank;
+    }
+
+    private boolean checkThreshold(Map<Long, Double> pagerank, Map<Long, Double> prTemp, double threshold) {
+        Iterator<Long> iterator = pagerank.keySet().iterator();
+        while (iterator.hasNext()) {
+            long nodeIdExt = iterator.next();
+            double diff = prTemp.get(nodeIdExt) - pagerank.get(nodeIdExt);
+            if (diff > threshold) {
+                return false;
+            }
+        }
+        return true;
     }
 
 }
--- src/main/java/com/graphaware/nlp/ml/textrank/TextRank.java
+++ src/main/java/com/graphaware/nlp/ml/textrank/TextRank.java
@@ -15,18 +15,15 @@
  */
 package com.graphaware.nlp.ml.textrank;
 
-import static com.graphaware.nlp.domain.Labels.AnnotatedText;
 import static com.graphaware.nlp.domain.Labels.Keyword;
 import static com.graphaware.nlp.domain.Relationships.DESCRIBES;
 import com.graphaware.nlp.util.MapUtil;
 import java.util.Arrays;
 import java.util.Comparator;
 import java.util.HashMap;
-import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
 import org.neo4j.graphdb.GraphDatabaseService;
 import org.neo4j.graphdb.Relationship;
 import org.neo4j.graphdb.ResourceIterator;
@@ -142,14 +139,10 @@ public class TextRank {
         return true;
     }
 
-    public boolean evaluate(Node annotatedText, Map<Long, Map<Long, CoOccurrenceItem>> coOccurrence, int iter, double damp) {
+    public boolean evaluate(Node annotatedText, Map<Long, Map<Long, CoOccurrenceItem>> coOccurrence, int iter, double damp, double threshold) {
         PageRank pageRank = new PageRank(database);
-        Map<Long, Double> pageRanks = pageRank.run(coOccurrence, iter, damp);
-        List<Map.Entry<Long, Double>> sortedPageRanks = MapUtil.getSortedListByValue(pageRanks);
-        int maxTop = pageRanks.size() > 10 ? 10 : pageRanks.size();
-        List<Long> top10 = sortedPageRanks.subList(0, maxTop).stream()
-                .map((item) -> item.getKey()).collect(Collectors.toList());
-
+        Map<Long, Double> pageRanks = pageRank.run(coOccurrence, iter, damp, threshold);
+        List<Long> top10 = getTop10(pageRanks);
         Map<String, Object> params = new HashMap<>();
         params.put("id", annotatedText.getId());
         params.put("nodeList", top10);
@@ -160,29 +153,34 @@ public class TextRank {
                 params);
         // First: merge neighboring words into phrases
         long prev_eP = -1000;
-        String lang = "";
         String keyword = "";
         Map<String, Integer> results = new HashMap<>();
         Map<String, Double> keywords = new HashMap<>();
         while (res != null && res.hasNext()) {
             Map<String, Object> next = res.next();
-            int sP = (int) next.get("sP");
-            int eP = (int) next.get("eP");
+            int startPosition = (int) next.get("sP");
+            int endPosition = (int) next.get("eP");
             Long tagId = (Long) next.get("tagId");
+            
             Double score = pageRanks.get(tagId);
-            //LOG.debug(tag + "\t" + sP + "\t" + eP + "\t" + score);
-
             String tag = (String) next.get("tag");
-            String tag_val = tag.split("_")[0];
-            if (tag.split("_").length > 2) {
+            
+            
+            final String[] tagSplit = tag.split("_");
+            if (tagSplit.length > 2) {
                 LOG.warn("Tag " + tag + " has more than 1 underscore symbols");
             }
-            if (removeStopWords && stopWords.stream().anyMatch(str -> str.equals(tag_val))) {
+            
+            String tagVal = tagSplit[0];
+            String lang = tagSplit[1];
+
+            if (removeStopWords && stopWords.stream().anyMatch(str -> str.equals(tagVal))) {
                 continue;
             }
-
-            if (sP - prev_eP <= 1) {
-                keyword += " " + tag_val;
+            keywords.put(tag, score);
+            
+            if (startPosition - prev_eP <= 1) {
+                keyword += " " + tagVal;
             } else {
                 if (keyword.split(" ").length > 1) {
                     if (results.containsKey(keyword + "_" + lang)) {
@@ -191,15 +189,13 @@ public class TextRank {
                         results.put(keyword + "_" + lang, 1);
                     }
                 }
-                keyword = tag_val;
-                lang = tag.split("_")[1];
+                keyword = tagVal;
             }
-            prev_eP = eP;
-            keywords.put(tag, score);
+            prev_eP = endPosition;
+            
         }
 
         // Next: include into final result simply top-x single words
-        LOG.debug("--- Keywords ---");
         keywords.entrySet().stream().sorted(Map.Entry.comparingByValue(Comparator.reverseOrder()))
                 .limit(8)
                 .forEach(en -> {
@@ -223,7 +219,8 @@ public class TextRank {
                     newNode = database.createNode(Keyword);
                     newNode.setProperty("id", key);
                     newNode.setProperty("value", val);
-                }   if (newNode != null) {
+                }
+                if (newNode != null) {
                     Relationship rel = newNode.createRelationshipTo(annotatedText, DESCRIBES);
                     rel.setProperty("count", results.get(key));
                 }
@@ -233,140 +230,11 @@ public class TextRank {
         return true;
     }
 
-//    public boolean evaluateOld(Node annotatedText, String relType, String relWeight, Integer iter, Double damp) {
-//        Map<String, Object> params = new HashMap<>();
-//        params.put("id", annotatedText.getId());
-//        params.put("rel", relType);
-//        params.put("relw", relWeight);
-//        params.put("iter", iter);
-//        params.put("damp", damp);
-//        Result res;
-//        try (Transaction tx = database.beginTx();) {
-//            res = database.execute(
-//                    "CALL ga.nlp.ml.textrank.computePageRank({annotatedID: {id}, relationshipType: \"" + relType + "\", relationshipWeight: \" " + relWeight + "\","
-//                    + " iter: {iter}, damp: {damp}}) YIELD node, score, node_w\n"
-//                    + "WITH node, score, node_w\n"
-//                    + "ORDER BY score DESC\n"
-//                    + "LIMIT 10\n"
-//                    + "MATCH (node)<-[:TAG_OCCURRENCE_TAG]-(to:TagOccurrence)<-[:SENTENCE_TAG_OCCURRENCE]-(:Sentence)<-[:CONTAINS_SENTENCE]-(a:AnnotatedText)\n"
-//                    + "WHERE a.id = {id}\n"
-//                    + "RETURN node.id as tag, to.startPosition as sP, to.endPosition as eP, score, node_w\n"
-//                    + "ORDER BY sP\n",
-//                    params);
-//            tx.success();
-//        } catch (Exception e) {
-//            LOG.error("evaluate(): calculating PageRank failed with QueryExecutionException: " + e.getMessage());
-//            return false;
-//        }
-//
-//        // First: merge neighboring words into phrases
-//        long prev_eP = -1000;
-//        String lang = "";
-//        String keyword = "";
-//        Map<String, Integer> results = new HashMap<>();
-//        Map<String, Double> keywords = new HashMap<>();
-//        while (res != null && res.hasNext()) {
-//            Map<String, Object> next = res.next();
-//            int sP = (int) next.get("sP");
-//            int eP = (int) next.get("eP");
-//            Double score = (Double) next.get("score");
-//            //LOG.debug(tag + "\t" + sP + "\t" + eP + "\t" + score);
-//
-//            String tag = (String) next.get("tag");
-//            String tag_val = tag.split("_")[0];
-//            if (tag.split("_").length > 2) {
-//                LOG.warn("Tag " + tag + " has more than 1 underscore symbols");
-//            }
-//            if (removeStopWords && stopWords.stream().anyMatch(str -> str.equals(tag_val))) {
-//                continue;
-//            }
-//
-//            if (sP - prev_eP <= 1) {
-//                keyword += " " + tag_val;
-//            } else {
-//                if (keyword.split(" ").length > 1) {
-//                    if (results.containsKey(keyword + "_" + lang)) {
-//                        results.put(keyword + "_" + lang, results.get(keyword + "_" + lang) + 1);
-//                    } else {
-//                        results.put(keyword + "_" + lang, 1);
-//                    }
-//                }
-//                keyword = tag_val;
-//                lang = tag.split("_")[1];
-//            }
-//            prev_eP = eP;
-//            keywords.put(tag, score);
-//        }
-//
-//        // Next: include into final result simply top-x single words
-//        LOG.debug("--- Keywords ---");
-//        keywords.entrySet().stream().sorted(Map.Entry.comparingByValue(Comparator.reverseOrder()))
-//                .limit(8)
-//                .forEach(en -> {
-//                    results.put(en.getKey(), 1);
-//                    LOG.debug(en.getKey());
-//                });
-//
-//        // prepare query for storing results into the graph
-//        /*String query = "MATCH (a:AnnotatedText) WHERE a.id = {id}\n WITH a\n";
-//        int c = 1;
-//        for (String key: results.keySet()) {
-//            if (key.split("_").length>2)
-//                LOG.warn("Tag " + key + " has more than 1 underscore symbols, newly created Keyword node might be wrong");
-//            String val = key.split("_")[0];
-//            if (removeStopWords && stopWords.stream().anyMatch(str -> str.equals(val)))
-//                continue;
-//            query += "MERGE (k" + c + ":Keyword {id: \"" + key + "\", value: \"" + val + "\"})\n";
-//            query += "MERGE (k" + c + ")-[r" + c + ":DESCRIBES {count: " + results.get(key) + "}]->(a)\nON CREATE SET r" + c + ".count=" + results.get(key) + " ON MATCH SET r" + c + ".count=" + results.get(key);
-//            c++;
-//        }
-//
-//        // store results into the graph
-//        try (Transaction tx = database.beginTx();) {
-//            LOG.debug("Query for storing results: \n" + query);
-//            database.execute(query, params);
-//            tx.success();
-//        } catch (Exception e) {
-//            LOG.error("evaluate(): storing results into graph failed with QueryExecutionException: " + e.getMessage());
-//            return false;
-//        }*/
-//        ResourceIterator<Node> findNodes = database.findNodes(AnnotatedText, "id", annotatedID);
-//        Node ann;
-//        if (findNodes.hasNext()) {
-//            ann = findNodes.next();
-//        } else {
-//            LOG.error("Error. AnnotatedText node with id " + annotatedID + " not found, aborting saving results into the graph.");
-//            return false;
-//        }
-//
-//        try (Transaction tx = database.beginTx();) {
-//            for (String key : results.keySet()) {
-//                if (key.split("_").length > 2) {
-//                    LOG.warn("Tag " + key + " has more than 1 underscore symbols, newly created Keyword node might be wrong");
-//                }
-//                String val = key.split("_")[0];
-//                if (removeStopWords && stopWords.stream().anyMatch(str -> str.equals(val))) {
-//                    continue;
-//                }
-//
-//                Node newNode;
-//                findNodes = database.findNodes(Keyword, "id", key);
-//                if (findNodes.hasNext()) {
-//                    newNode = findNodes.next();
-//                } else {
-//                    newNode = database.createNode(Keyword);
-//                    newNode.setProperty("id", key);
-//                    newNode.setProperty("value", val);
-//                }
-//
-//                if (newNode != null) {
-//                    Relationship rel = newNode.createRelationshipTo(ann, DESCRIBES);
-//                    rel.setProperty("count", results.get(key));
-//                }
-//            }
-//            tx.success();
-//        }
-//
-//        return true;
-//    }
+    private List<Long> getTop10(Map<Long, Double> pageRanks) {
+        List<Map.Entry<Long, Double>> sortedPageRanks = MapUtil.getSortedListByValue(pageRanks);
+        int maxTop = pageRanks.size() > 10 ? 10 : pageRanks.size();
+        List<Long> top10 = sortedPageRanks.subList(0, maxTop).stream()
+                .map((item) -> item.getKey()).collect(Collectors.toList());
+        return top10;
+    }
 }
