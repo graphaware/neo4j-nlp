e3305e4ee3c05a36647daf721fe0dd8ef4d5822e
Christophe Willemsen <willemsen.christophe@gmail.com>
2017-08-31 01:16:22 +0400

    added test for sentiment procedure

--- src/main/java/com/graphaware/nlp/dsl/procedure/SentimentProcedure.java
+++ src/main/java/com/graphaware/nlp/dsl/procedure/SentimentProcedure.java
@@ -4,6 +4,7 @@ import com.graphaware.nlp.dsl.AbstractDSL;
 import com.graphaware.nlp.dsl.result.SingleResult;
 import org.neo4j.graphdb.Node;
 import org.neo4j.procedure.Description;
+import org.neo4j.procedure.Mode;
 import org.neo4j.procedure.Name;
 import org.neo4j.procedure.Procedure;
 
@@ -11,7 +12,7 @@ import java.util.stream.Stream;
 
 public class SentimentProcedure extends AbstractDSL {
 
-    @Procedure(name = "ga.nlp.sentiment")
+    @Procedure(name = "ga.nlp.sentiment", mode = Mode.WRITE)
     @Description("Apply sentiment extraction on the given annotated text")
     public Stream<SingleResult> applySentiment(@Name("annotatedText") Node annotatedText, @Name(value = "textProcessor", defaultValue = "") String textProcessor) {
         getNLPManager().applySentiment(annotatedText, textProcessor);
