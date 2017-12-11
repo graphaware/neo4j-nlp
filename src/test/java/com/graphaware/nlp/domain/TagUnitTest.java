565fff44dab43a8e20b86e6740ea9bf1f6ba8d39
Christophe Willemsen <willemsen.christophe@gmail.com>
2017-12-11 13:19:29 +0100

    assert tags are trimmed

--- src/test/java/com/graphaware/nlp/domain/TagUnitTest.java
+++ src/test/java/com/graphaware/nlp/domain/TagUnitTest.java
@@ -1,6 +1,14 @@
 package com.graphaware.nlp.domain;
 
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
 public class TagUnitTest {
 
-//    @Test
+    @Test
+    public void testTagIsTrimmedOnConstruct() {
+        Tag tag = new Tag(" Institute", "en");
+        assertEquals("institute", tag.getLemma());
+    }
 }
