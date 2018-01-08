83bab25ed4d67f07907d1d815ed1760aded27023
Vlasta Kus <vlastimil.kus@gmail.com>
2018-01-08 10:12:41 +0100

    Change in PipelineSpecification.

--- src/main/java/com/graphaware/nlp/dsl/request/PipelineSpecification.java
+++ src/main/java/com/graphaware/nlp/dsl/request/PipelineSpecification.java
@@ -140,7 +140,7 @@ public class PipelineSpecification {
         else if (obj instanceof String)
             result = !((String) obj).isEmpty();
         else if (obj instanceof Number)
-            result = ((Number) obj).doubleValue() != 0.0d;
+            result = true; //((Number) obj).doubleValue() != 0.0d;
         return result;
     }
 
