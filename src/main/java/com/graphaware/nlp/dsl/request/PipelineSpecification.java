1a408aea76539485cb961acc02881dea46443a87
Christophe Willemsen <willemsen.christophe@gmail.com>
2018-01-08 11:06:12 +0100

    fixed conflicts

--- src/main/java/com/graphaware/nlp/dsl/request/PipelineSpecification.java
+++ src/main/java/com/graphaware/nlp/dsl/request/PipelineSpecification.java
@@ -19,6 +19,7 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 import static com.graphaware.nlp.dsl.request.RequestConstants.*;
 
