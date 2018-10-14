5d18f0f2e9e5109cce800c6b84f28b81ee8a99bd
Christophe Willemsen <willemsen.christophe@gmail.com>
2018-10-14 14:40:11 +0200

    throw exception if n/a is passed to the conceptnet5 client

--- src/main/java/com/graphaware/nlp/enrich/conceptnet5/ConceptNet5Client.java
+++ src/main/java/com/graphaware/nlp/enrich/conceptnet5/ConceptNet5Client.java
@@ -32,6 +32,7 @@ import java.util.concurrent.TimeUnit;
 
 public class ConceptNet5Client {
     private static final Log LOG = LoggerFactory.getLogger(ConceptNet5Client.class);
+    private static final String FORBIDDEN_LANGUAGE = "n/a";
 
     private final String conceptNet5EndPoint;
     private final ClientConfig cfg;
@@ -84,6 +85,10 @@ public class ConceptNet5Client {
     }
 
     public ConceptNet5EdgeResult queryBy(String direction, String concept, String rel, String lang, int limit) {
+        if (lang.equalsIgnoreCase(FORBIDDEN_LANGUAGE)) {
+            throw new RuntimeException("Unsupported language " + lang + ", maybe you forgot to set a default language in " +
+                    "your configuration or specify it in the enrich request ?");
+        }
         String url = conceptNet5EndPoint + "/query?rel=/r/" + rel + "&" + direction + "=/c/" + lang + "/" + concept + "&limit=" + limit;
         ConceptNet5EdgeResult value;
         try {
